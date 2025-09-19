318f3b2b6c9dd06472f08c1c0195eddbdeb2668a
Asit D <asitdeva@gmail.com>
2025-09-19 17:53:53 -0400

    website ver 1

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>Welcome to Storylineds Website</h1>
+      <p>This is a static website with animations and graphics.</p>
+    </div>
+  );
+}
+
+export default Home;
